917382a11f92bdc902d560e9852be5f8087ea21b
Lay <kmr345@naver.com>
2023-01-10 21:52:20 +0900

    230110

--- thisiscodingtest/1/21_3.py
+++ thisiscodingtest/1/21_3.py
@@ -1,46 +1,48 @@
 from collections import deque
 
-n, l, r = map(int, input().split())
+input = """
+4 10 50
+10 100 20 90
+80 100 60 70
+70 20 30 40
+50 20 100 10
+""".strip().split('\n')
 
-graph = []
-for _ in range(n):
-    graph.append(list(map(int, input().split())))
+n, l, r = map(int, input[0].split())
+graph = [list(map(int, row.split())) for row in input[1:]]
 
-dx = [-1, 0, 1, 0]
-dy = [0, -1, 0, 1]
-
-result = 0
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
 
 
 def process(x, y, index):
     united = []
     united.append((x, y))
-    q = deque()
-    q.append((x, y))
+    queue = deque()
+    queue.append((x, y))
 
     union[x][y] = index
     summary = graph[x][y]
     count = 1
-    while q:
-        # print(union)
-        x, y = q.popleft()
+    while queue:
+        x, y = queue.popleft()
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
             if 0 <= nx < n and 0 <= ny < n and union[nx][ny] == -1:
-                if l <= abs(graph[nx][ny] - graph[x][y]) <= r:
-                    q.append((nx, ny))
+                if l < abs(graph[nx][ny] - graph[x][y]):
+                    queue.append((nx, ny))
                     union[nx][ny] = index
                     summary += graph[nx][ny]
                     count += 1
                     united.append((nx, ny))
-    for i, j in united:
-        graph[i][j] = summary // count
+
+    for x, y in united:
+        graph[x][y] = summary // count
     return count
 
 
-total_count = 0
-
+total_index = 0
 while True:
     union = [[-1] * n for _ in range(n)]
     index = 0
@@ -49,9 +51,10 @@ while True:
             if union[i][j] == -1:
                 process(i, j, index)
                 index += 1
-
     if index == n * n:
         break
-    total_count += 1
-    print(union)
-print(total_count)
+    total_index += 1
+print(total_index)
+
+print(n, l, r)
+print(graph)
--- thisiscodingtest/1/21_4.py
+++ thisiscodingtest/1/21_4.py
@@ -1,45 +1,48 @@
 from collections import deque
 
-n, l, r = map(int, input().split())
+input = """
+4 10 50
+10 100 20 90
+80 100 60 70
+70 20 30 40
+50 20 100 10
+""".strip().split('\n')
 
-graph = []
-for _ in range(n):
-    graph.append(list(map(int, input().split())))
+n, l, r = map(int, input[0].split())
+graph = [list(map(int, raw.split())) for raw in input[1:]]
 
-dx = [-1, 0, 1, 0]
-dy = [0, -1, 0, 1]
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
 
-result = 0
+total_count = 0
 
 
 def process(x, y, index):
     united = []
     united.append((x, y))
-    q = deque()
-    q.append((x, y))
+    queue = deque()
+    queue.append((x, y))
 
-    union[x][y] = index
     summary = graph[x][y]
     count = 1
-    while q:
-        # print(union)
-        x, y = q.popleft()
+    union[x][y] = index  ###
+
+    while queue:
+        x, y = queue.popleft()
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
             if 0 <= nx < n and 0 <= ny < n and union[nx][ny] == -1:
                 if l <= abs(graph[nx][ny] - graph[x][y]) <= r:
-                    q.append((nx, ny))
-                    union[nx][ny] = index
-                    summary += graph[nx][ny]
-                    count += 1
                     united.append((nx, ny))
+                    queue.append((nx, ny))
+                    count += 1
+                    summary += graph[nx][ny]
+                    union[nx][ny] = index
     for i, j in united:
         graph[i][j] = summary // count
-    return count
+    return index
 
-
-total_count = 0
 
 while True:
     union = [[-1] * n for _ in range(n)]
@@ -49,9 +52,7 @@ while True:
             if union[i][j] == -1:
                 process(i, j, index)
                 index += 1
-
     if index == n * n:
         break
     total_count += 1
-    print(union)
 print(total_count)
