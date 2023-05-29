d6bab76d2cfbc7345be2bead575beb85eb5e1a39
kim hyoung lae <31833270+Lay4U@users.noreply.github.com>
2023-05-29 21:36:27 +0900

    [Silver II] Title: 마인크래프트, Time: 144 ms, Memory: 36216 KB -BaekjoonHub

---
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.py
@@ -0,0 +1,28 @@
+import sys
+from collections import Counter
+
+n, m, b = map(int, sys.stdin.readline().split())
+land = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+
+flat_land = [height for row in land for height in row]
+counter = Counter(flat_land)
+
+min_time = float('inf')
+max_height = 0
+
+for h in range(257):
+    time = 0
+    inventory = b
+    for height, freq in counter.items():
+        if height < h:
+            time += (h - height) * freq
+            inventory -= (h - height) * freq
+        else:
+            time += (height - h) * 2 * freq
+            inventory += (height - h) * freq
+    if inventory >= 0:
+        if time <= min_time:
+            min_time = time
+            max_height = h
+
+print(min_time, max_height)
