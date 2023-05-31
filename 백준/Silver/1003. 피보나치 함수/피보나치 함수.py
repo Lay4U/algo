b321ca71d2e5de9f3e7d750f3a9c73c9a0327e90
kim hyoung lae <31833270+Lay4U@users.noreply.github.com>
2023-05-31 21:36:22 +0900

    [Silver III] Title: 피보나치 함수, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -1,16 +1,13 @@
 import sys
+
 input = sys.stdin.readline
-
-def fibo_dp(n):
-    fibo_counts = [(1, 0), (0, 1)]
-
-    for i in range(2, n + 1):
-        zeros, ones = map(sum, zip(fibo_counts[i - 1], fibo_counts[i - 2]))
-        fibo_counts.append((zeros, ones))
-    return fibo_counts[n]
-
+fibo_counts = [(1, 0), (0, 1)]
+for i in range(2, 50):
+    ones, zeros = map(sum, zip(fibo_counts[i - 1], fibo_counts[i - 2]))
+    fibo_counts.append((ones, zeros))
+    # print(i, ones, zeros, '\t', fibo_counts)
 
 T = int(input())
 for _ in range(T):
-    N = int(input())
-    print(*fibo_dp(N))
+    n = int(input())
+    print(*fibo_counts[n])
