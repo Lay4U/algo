ea4f6348563925c5555e66f7a95456a495a81442
kim hyoung lae <31833270+Lay4U@users.noreply.github.com>
2023-06-05 21:25:32 +0900

    [Silver II] Title: 유기농 배추, Time: 372 ms, Memory: 31620 KB -BaekjoonHub

---
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -0,0 +1,34 @@
+import sys
+sys.setrecursionlimit(10**6)
+
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
+
+
+def dfs(x, y):
+    visited[x][y] = True
+
+    for i in range(4):
+        nx, ny = x + dx[i], y + dy[i]
+        if nx < 0 or nx >= n or ny < 0 or ny >= m:
+            continue
+        elif field[nx][ny] and not visited[nx][ny]:
+            dfs(nx, ny)
+
+
+for _ in range(int(input())):
+    m, n, k = map(int, input().split())
+    field = [[0] * m for _ in range(n)]
+    visited = [[False] * m for _ in range(n)]
+    for _ in range(k):
+        y, x = map(int, input().split())
+        field[x][y] = 1
+
+    answer = 0
+    for i in range(n):
+        for j in range(m):
+            if field[i][j] and not visited[i][j]:
+                dfs(i, j)
+                answer += 1
+
+    print(answer)
