e44f13e99b0c2497a183c18c291c27a3c48fb083
kim hyoung lae <31833270+Lay4U@users.noreply.github.com>
2023-06-23 20:51:30 +0900

    [Silver II] Title: 유기농 배추, Time: 292 ms, Memory: 31620 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,27 +1,25 @@
-from collections import deque
+import sys
+
+sys.setrecursionlimit(10 ** 7)
 
 dx = [1, -1, 0, 0]
-dy = [0, 0, 1, -1]
+dy = [0, 0, -1, 1]
 
 
-def bfs(x, y):
-    queue = deque([(x, y)])
-    while queue:
-        x, y = queue.popleft()
-        for i in range(4):
-            nx, ny = x + dx[i], y + dy[i]
-            if nx < 0 or nx >= n or ny < 0 or ny >= m:
-                continue
-            elif field[nx][ny] and not visited[nx][ny]:
-                queue.append((nx, ny))
-                visited[nx][ny] = True
+def dfs(x, y):
+    visited[x][y] = True
+    for i in range(4):
+        nx, ny = x + dx[i], y + dy[i]
+        if nx < 0 or nx >= n or ny < 0 or ny >= m:
+            continue
+        elif field[nx][ny] == 1 and not visited[nx][ny]:
+            dfs(nx, ny)
 
 
 for _ in range(int(input())):
     m, n, k = map(int, input().split())
     field = [[0] * m for _ in range(n)]
     visited = [[False] * m for _ in range(n)]
-
     for _ in range(k):
         y, x = map(int, input().split())
         field[x][y] = 1
@@ -29,8 +27,8 @@ for _ in range(int(input())):
     answer = 0
     for i in range(n):
         for j in range(m):
-            if field[i][j] and not visited[i][j]:
-                bfs(i, j)
+            if field[i][j] == 1 and not visited[i][j]:
                 answer += 1
+                dfs(i, j)
 
     print(answer)
