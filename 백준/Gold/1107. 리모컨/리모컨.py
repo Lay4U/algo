b2ea7b7515c358c64c2df584dbbbd39ce0db51d0
kim hyoung lae <31833270+Lay4U@users.noreply.github.com>
2023-06-25 21:10:56 +0900

    [Gold V] Title: 리모컨, Time: 2196 ms, Memory: 31388 KB -BaekjoonHub

--- 백준/Gold/1107. 리모컨/리모컨.py
+++ 백준/Gold/1107. 리모컨/리모컨.py
@@ -1,15 +1,14 @@
 n = int(input())
 m = int(input())
-broken_buttons = set(input().split()) if m else set()
+broken = set(input()) if m else set()
 
 answer = abs(n - 100)
 
 for channel in range(1000001):
     for j in range(len(str(channel))):
-        if str(channel)[j] in broken_buttons:
+        if str(channel)[j] in broken:
             break
     else:
-        answer = min(answer, abs(n - channel) + len(str(channel)))
-        # print(str(channel)[j], channel, j, answer)
+        answer = min(answer, abs(channel - n) + len(str(channel)))
 
 print(answer)
