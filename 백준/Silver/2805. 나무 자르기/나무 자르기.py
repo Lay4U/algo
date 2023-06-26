bde249bfbf7a35547231b0eebaad6a8cb0e0132a
kim hyoung lae <31833270+Lay4U@users.noreply.github.com>
2023-06-26 21:22:47 +0900

    [Silver II] Title: 나무 자르기, Time: 2092 ms, Memory: 150336 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -5,7 +5,6 @@ start, end = 0, max(datas)
 
 while start <= end:
     mid = (start + end) // 2
-
     total = sum(data - mid if data > mid else 0 for data in datas)
     if total >= m:
         start = mid + 1
