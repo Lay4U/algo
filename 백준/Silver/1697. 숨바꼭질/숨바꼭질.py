774aa4974a9913764dd418d2fbda3929e5a78d1b
kim hyoung lae <31833270+Lay4U@users.noreply.github.com>
2023-06-27 21:25:04 +0900

    [Silver I] Title: 숨바꼭질, Time: 452 ms, Memory: 71888 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -0,0 +1,19 @@
+from collections import deque
+
+def bfs(n, k):
+    MAX = 1000001
+    times = [0] * MAX
+    queue = deque([n])
+    while queue:
+        v = queue.popleft()
+        if v == k:
+            return times[v]
+
+        for next_v in (v - 1, v + 1, v * 2):
+            if 0 <= next_v < MAX and not times[next_v]:
+                times[next_v] = times[v] + 1
+                queue.append(next_v)
+
+
+n, k = map(int, input().split())
+print(bfs(n, k))
