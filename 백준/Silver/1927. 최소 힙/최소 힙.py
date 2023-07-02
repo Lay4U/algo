1624beecfe182569bf553e3b858bfb40459a6f8e
kim hyoung lae <31833270+Lay4U@users.noreply.github.com>
2023-07-02 21:45:48 +0900

    [Silver II] Title: 최소 힙, Time: 124 ms, Memory: 37180 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.py
+++ 백준/Silver/1927. 최소 힙/최소 힙.py
@@ -1,6 +1,5 @@
 import heapq
 import sys
-from collections import deque
 
 input = sys.stdin.readline
 
@@ -9,10 +8,7 @@ n = int(input())
 heap = []
 for _ in range(n):
     data = int(input())
-    if data == 0:
-        if heap:
-            print(heapq.heappop(heap))
-        else:
-            print(0)
-    else:
+    if data:
         heapq.heappush(heap, data)
+    else:
+        print(heapq.heappop(heap) if heap else 0)
