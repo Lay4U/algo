c0bdfedad77ed33f049384e56cb4c6fc408d75fb
kim hyoung lae <31833270+Lay4U@users.noreply.github.com>
2023-07-05 21:35:04 +0900

    [Silver I] Title: 회의실 배정, Time: 4008 ms, Memory: 52036 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -2,11 +2,11 @@ n = int(input())
 meetings = [tuple(map(int, input().split())) for _ in range(n)]
 meetings.sort(key=lambda x: (x[1], x[0]))
 
-last_end = 0
+last_time = 0
 count = 0
 for start, end in meetings:
-    if start >= last_end:
+    if start >= last_time:
         count += 1
-        last_end = end
+        last_time = end
 
 print(count)
