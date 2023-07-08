7d39897fbbf5b06cabf4b9ea53cd7ec7958fd7eb
kim hyoung lae <31833270+Lay4U@users.noreply.github.com>
2023-07-08 21:38:10 +0900

    [Silver I] Title: 회의실 배정, Time: 224 ms, Memory: 52036 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -1,9 +1,13 @@
+import sys
+
+input = sys.stdin.readline
 n = int(input())
-meetings = [tuple(map(int, input().split())) for _ in range(n)]
+meetings = [(tuple(map(int, input().split()))) for _ in range(n)]
 meetings.sort(key=lambda x: (x[1], x[0]))
 
-last_time = 0
 count = 0
+last_time = 0
+
 for start, end in meetings:
     if start >= last_time:
         count += 1
