0d2dbc3c54811e6f46df57708e6430b60a67855d
kim hyoung lae <31833270+Lay4U@users.noreply.github.com>
2023-07-10 21:33:36 +0900

    [Silver I] Title: Z, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1074. Z/Z.py
+++ 백준/Silver/1074. Z/Z.py
@@ -1,20 +1,19 @@
-def z_order(n, r, c):
+def zindex(n, r, c):
     if n == 0:
         return 0
-
     half = 2 ** (n - 1)
 
     if r < half:
         if c < half:
-            return z_order(n - 1, r, c)
+            return zindex(n - 1, r, c)
         else:
-            return half ** 2 + z_order(n - 1, r, c - half)
+            return half ** 2 + zindex(n - 1, r, c - half)
     else:
         if c < half:
-            return 2 * half ** 2 + z_order(n - 1, r - half, c)
+            return 2 * half ** 2 + zindex(n - 1, r - half, c)
         else:
-            return 3 * half ** 2 + z_order(n - 1, r - half, c - half)
+            return 3 * half ** 2 + zindex(n - 1, r - half, c - half)
 
 
 n, r, c = map(int, input().split())
-print(z_order(n, r, c))
+print(zindex(n, r, c))
