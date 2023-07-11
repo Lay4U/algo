852dd64750939b52b9d3e9e6dfb7248247e1e9cb
kim hyoung lae <31833270+Lay4U@users.noreply.github.com>
2023-07-11 21:24:25 +0900

    [Silver I] Title: 미로 탐색, Time: 84 ms, Memory: 34140 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,19 +1,19 @@
 from collections import deque
 
 n, m = map(int, input().split())
-maze = [list(map(int, input())) for _ in range(n)]
+graph = [[int(value) for value in input()] for _ in range(n)]
 
-dx = [1, -1, 0, 0]
-dy = [0, 0, 1, -1]
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
 queue = deque([(0, 0)])
 while queue:
     x, y = queue.popleft()
     for i in range(4):
         nx, ny = x + dx[i], y + dy[i]
-        if nx < 0 or nx >= n or ny < 0 or ny >= m:
-            continue
-        if maze[nx][ny] == 1:
-            maze[nx][ny] = maze[x][y] + 1
-            queue.append((nx, ny))
-print(maze[n - 1][m - 1])
+        if 0 <= nx < n and 0 <= ny < m:
+            if graph[nx][ny] == 1:
+                queue.append((nx, ny))
+                graph[nx][ny] = graph[x][y] + 1
+
+print(graph[n - 1][m - 1])
