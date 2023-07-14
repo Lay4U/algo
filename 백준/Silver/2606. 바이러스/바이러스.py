fa5e6453cef909a9de97b43b3ff182bf67d0a889
kim hyoung lae <31833270+Lay4U@users.noreply.github.com>
2023-07-14 21:40:34 +0900

    [Silver III] Title: 바이러스, Time: 68 ms, Memory: 34112 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -3,20 +3,21 @@ from collections import deque
 n = int(input())
 m = int(input())
 graph = [[] for _ in range(n + 1)]
-visited = [False] * (n + 1)
 for _ in range(m):
     a, b = map(int, input().split())
     graph[a].append(b)
     graph[b].append(a)
 
+visited = [False] * (n + 1)
 visited[1] = True
-count = 0
 queue = deque([1])
+count = 0
 while queue:
     v = queue.popleft()
-    for next in graph[v]:
-        if not visited[next]:
-            queue.append(next)
-            visited[next] = True
+    for v in graph[v]:
+        if not visited[v]:
             count += 1
+            queue.append(v)
+            visited[v] = True
+
 print(count)
