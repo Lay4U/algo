2e3a7811ddbb3c4e9e0c3a61082894569b0d99db
kim hyoung lae <31833270+Lay4U@users.noreply.github.com>
2023-07-26 21:10:50 +0900

    [Silver II] Title: 색종이 만들기, Time: 56 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
@@ -1,27 +1,27 @@
-import sys
-input = sys.stdin.readline
-
-def paper(x, y, n):
-    global graph, white, blue
-    color = graph[x][y]
+def count_paper(x, y, n):
+    global paper, white, blue
+    color = paper[x][y]
     half = n // 2
+
     for i in range(x, x + n):
         for j in range(y, y + n):
-            if graph[i][j] != color:
-                paper(x, y, half)
-                paper(x + half, y, half)
-                paper(x, y + half, half)
-                paper(x + half, y + half, half)
+            if paper[i][j] != color:
+                count_paper(x, y, half)
+                count_paper(x + half, y, half)
+                count_paper(x, y + half, half)
+                count_paper(x + half, y + half, half)
                 return
+
     if color == 0:
         white += 1
-    elif color == 1:
+    else:
         blue += 1
 
 
 n = int(input())
-graph = [list(map(int, input().split())) for _ in range(n)]
+paper = [list(map(int, input().split())) for _ in range(n)]
+
 white, blue = 0, 0
-paper(0, 0, n)
+count_paper(0, 0, n)
 print(white)
 print(blue)
