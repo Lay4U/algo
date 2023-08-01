972b03e095b186f3b2f4447547fe2f65122d068b
kim hyoung lae <31833270+Lay4U@users.noreply.github.com>
2023-08-01 21:16:52 +0900

    [Gold V] Title: 토마토, Time: 2424 ms, Memory: 98596 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -1,7 +1,6 @@
 from collections import deque
 
 m, n = map(int, input().split())
-
 tomato = [list(map(int, input().split())) for _ in range(n)]
 
 queue = deque()
@@ -9,13 +8,12 @@ for i in range(n):
     for j in range(m):
         if tomato[i][j] == 1:
             queue.append((i, j))
-
 dx = [1, -1, 0, 0]
 dy = [0, 0, 1, -1]
 while queue:
     x, y = queue.popleft()
     for i in range(4):
-        nx, ny = x + dx[i], y + dy[i]
+        nx, ny = dx[i] + x, dy[i] + y
         if 0 <= nx < n and 0 <= ny < m:
             if tomato[nx][ny] == 0:
                 tomato[nx][ny] = tomato[x][y] + 1
@@ -27,7 +25,7 @@ for i in range(n):
         if tomato[i][j] == 0:
             print(-1)
             exit()
-
-        result = max(result, tomato[i][j])
+        else:
+            result = max(result, tomato[i][j])
 
 print(result - 1)
