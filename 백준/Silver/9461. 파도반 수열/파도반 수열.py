01750202b60c3bbd9e143e1d3b4771108277d98f
kim hyoung lae <31833270+Lay4U@users.noreply.github.com>
2023-08-08 21:43:20 +0900

    [Silver III] Title: 파도반 수열, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.py
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.py
@@ -1,9 +1,10 @@
 import sys
 input = sys.stdin.readline
 
+p = [1, 1, 1]
+
+for i in range(3, 101):
+    p.append(p[i - 3] + p[i - 2])
+
 for _ in range(int(input())):
-    n = int(input())
-    p = [0, 1, 1, 1]
-    for i in range(4, n + 1):
-        p.append(p[i - 2] + p[i - 3])
-    print(p[n])
+    print(p[int(input()) - 1])
