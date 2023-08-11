de4180ad55c4b973211af62f0bc9675d19960bb3
kim hyoung lae <31833270+Lay4U@users.noreply.github.com>
2023-08-11 21:20:09 +0900

    [Silver IV] Title: 동전 0, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.py
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -1,11 +1,9 @@
 n, k = map(int, input().split())
-
 coins = [int(input()) for _ in range(n)]
-coins.sort(reverse=True)
 count = 0
-for coin in coins:
+for coin in reversed(coins):
     count += k // coin
-    k %= coin
+    k = k % coin
     if k == 0:
         break
 
